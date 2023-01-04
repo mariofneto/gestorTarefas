6052a5e9df31ff9cb613da31e50f99e22dc37ed5
Mário Ferreira <marioo.netoo3@gmail.com>
2023-01-04 10:02:21 -0300

    projeto criado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gestorTarefas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
